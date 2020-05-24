39620b1e9040115a211496c74baeff56ef98fb1e
9ou9ou <47674678+9ou9ou@users.noreply.github.com>
2020-05-24 18:25:47 +0200

    forgotPassword && contact zone

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Form\ResetPassType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -17,6 +23,7 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        //if the user is logged, we make sure that he can't access the loginPage
          if ($this->getUser()) {
              return $this->redirectToRoute('home');
          }
@@ -36,5 +43,92 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+      /**
+       * The function make sure to send a email if the user clicked forgotPassword
+       * @Route("/forgotPassword", name="app_forgotten_password")
+       */
+    public function forgotPassword (Request $request, UserRepository $users, \Swift_Mailer $mailer, 
+                                    TokenGeneratorInterface $tokenGenerator): Response {
+
+         $form = $this->createForm(ResetPassType::class);
+         $form->handleRequest($request);
+
+         if ($form->isSubmitted() && $form->isValid()) {
+             //Recover the data in the form
+            $donnees = $form->getData();
+            $user = $users->findOneByEmail($donnees['email']);
+            // if the user doesn't exist
+             if ($user === null) {
+
+            $this->addFlash('danger', 'This mail adress is unknown');
+            return $this->redirectToRoute('app_login');
+        }
+          // Generate a token form initialition of the password
+           $token = $tokenGenerator->generateToken();
+
+        try{
+            $user->setResetToken($token);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        } catch (\Exception $e) {
+            $this->addFlash('warning', $e->getMessage());
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Generate the URL for the reset of password
+        $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+
+        //handle the mail
+        $message = (new \Swift_Message('Mot de passe oublié'))
+            ->setFrom('koussayjebari2070@gmail.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+                "Bonjour,<br><br>Une demande de réinitialisation de mot de passe a été effectuée pour le site. Veuillez cliquer sur le lien suivant : " . $url."</p>",
+                'text/html'
+            )
+        ;
+        $mailer->send($message);
+
+        $this->addFlash('message', 'Password reset email sent!');
+        return $this->redirectToRoute('app_login');
+    }
+
+    return $this->render('security/forgotten_password.html.twig',[
+        'emailForm' => $form->createView(),
+        ]);
+}
+
+     /**
+      * the function handle the Password reseting
+     * @Route("/reset_pass/{token}", name="app_reset_password")
+      */
+     public function resetPassword(Request $request, string $token, 
+                                   UserPasswordEncoderInterface $passwordEncoder, UserRepository $userRepo) {
+        // We are looking for a user with the given token
+         $user = $userRepo->findOneBy(['reset_token' => $token]);
+            if ($user === null) {
+                 $this->addFlash('danger', 'Token Inconnu');
+                  return $this->redirectToRoute('app_login');
+                  }
+
+        //If the form is sent in post method
+           if ($request->isMethod('POST')) {
+               $user->setResetToken(null);
+               $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+               
+               $entityManager = $this->getDoctrine()->getManager();
+               $entityManager->persist($user);
+               $entityManager->flush();
+               
+               $this->addFlash('message', 'Mot de passe mis à jour');
+               return $this->redirectToRoute('app_login');
+            }else {
+        
+        return $this->render('security/reset_password.html.twig', ['token' => $token]);
+    }
+
+}
 
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -32,6 +32,11 @@ class User  implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $reset_token;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -85,6 +90,18 @@ class User  implements UserInterface
     public function eraseCredentials()
     {
         
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->reset_token;
+    }
+
+    public function setResetToken(?string $reset_token): self
+    {
+        $this->reset_token = $reset_token;
+
+        return $this;
     }
 
 }
